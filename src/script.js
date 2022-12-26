4247f0777868d76bebc0f1376c0fdf62c57e2947
Leonardo Carvalho <leonardoscc15@gmail.com>
2022-12-26 18:58:45 -0300

    [02-webpack] render a red cube

--- src/script.js
+++ src/script.js
@@ -1,1 +1,32 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+
+const canvas = document.querySelector(".webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Red Cube
+const cubeGeometry = new THREE.BoxGeometry();
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+});
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// WebGL Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
